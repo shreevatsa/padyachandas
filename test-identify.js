177ac73ef4a4e13521b74d675f442a066545e716
Shreevatsa R <shreevatsa.public@gmail.com>
2016-06-27 19:25:25 -0700

    Change deepEqual to deepStrictEqual, fix bugs.

--- test-identify.js
+++ test-identify.js
@@ -12,20 +12,20 @@ function test() {
   const line = '22221111122122122';
 
   const verseUnknown = ['112211', '111122', '122111', '121121'];
-  assert.deepEqual(identify(verseUnknown), {});
-  assert.deepEqual(identify(blobify(verseUnknown)), {});
+  assert.deepStrictEqual(identify(verseUnknown), {});
+  assert.deepStrictEqual(identify(blobify(verseUnknown)), {});
 
   const versePerfect = [line, line, line, line];
-  assert.deepEqual(identify(versePerfect), { exact: 'mandakranta' });
-  assert.deepEqual(identify(blobify(versePerfect)), { blobExact: 'mandakranta' });
+  assert.deepStrictEqual(identify(versePerfect), { exact: 'mandakranta' });
+  assert.deepStrictEqual(identify(blobify(versePerfect)), { blobExact: 'mandakranta' });
 
   // One or both of the even pAda-s ending in a laghu
   const lineLaghu = line.replace(/2$/, '1');
   for (const verseOkay of [[line, line, line, lineLaghu],
                            [line, lineLaghu, line, line],
                            [line, lineLaghu, line, lineLaghu]]) {
-    assert.deepEqual(identify(verseOkay), { okay: 'mandakranta' });
-    assert.deepEqual(identify(blobify(verseOkay), { okay: 'mandakranta' }));
+    assert.deepStrictEqual(identify(verseOkay), { okay: 'mandakranta' });
+    assert.deepStrictEqual(identify(blobify(verseOkay)), { blobOkay: 'mandakranta' });
   }
 
   // Also the *odd* pAda-s ending in a laghu. The remaining 16 - (1 + 3) cases.
@@ -41,8 +41,8 @@ function test() {
                        [lineLaghu, lineLaghu, line, lineLaghu],
                        [lineLaghu, lineLaghu, lineLaghu, line],
                        [lineLaghu, lineLaghu, lineLaghu, lineLaghu]]) {
-    assert.deepEqual(identify(verse), { paadaanta: 'mandakranta' });
-    assert.deepEqual(blobify(identify(verse)), { blobPaadaanta: 'mandakranta' });
+    assert.deepStrictEqual(identify(verse), { paadaanta: 'mandakranta' });
+    assert.deepStrictEqual(identify(blobify(verse)), { blobPaadaanta: 'mandakranta' });
   }
 }
 
